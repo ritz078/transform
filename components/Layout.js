3684eeffa8edd4cdac5bd115c4af4751967ef479
Ritesh Kumar <rkritesh078@gmail.com>
2017-09-24 00:40:32 +0530

    add codesponsor ad

--- components/Layout.js
+++ components/Layout.js
@@ -28,6 +28,11 @@ function Logo () {
       fill='#ffffff'
       width='190'
     >
+      <style jsx>{`
+        svg {
+          height: 125px;
+        }
+      `}</style>
       <path d='M68.6 34c-2.6 0-4.7 1.6-5.7 3.8l-25.4-6v-.4c0-3.4-2.8-6.2-6.2-6.2-3.4 0-6.2 2.8-6.2 6.2 0 3.1 2.2 5.6 5.2 6.1v25.1c-2.9.5-5.2 3-5.2 6.1 0 3.4 2.8 6.2 6.2 6.2 3.1 0 5.6-2.2 6.1-5.2h16.7c.5 2.9 3 5.2 6.1 5.2 3.4 0 6.2-2.8 6.2-6.2 0-2.4-1.4-4.4-3.3-5.5l4.8-16.9c.3 0 .6.1.9.1 3.4 0 6.2-2.8 6.2-6.2-.2-3.4-3-6.2-6.4-6.2zm0 10.3c-2.3 0-4.2-1.9-4.2-4.2s1.9-4.2 4.2-4.2c2.3 0 4.2 1.9 4.2 4.2s-1.9 4.2-4.2 4.2zM31.4 27.2c2.3 0 4.2 1.9 4.2 4.2s-1.9 4.2-4.2 4.2c-2.3 0-4.2-1.9-4.2-4.2s1.9-4.2 4.2-4.2zm4.1 41.4c0 2.3-1.9 4.2-4.2 4.2-2.3 0-4.2-1.9-4.2-4.2s1.9-4.2 4.2-4.2c2.4.1 4.2 1.9 4.2 4.2zm24.7 4.2c-2.3 0-4.2-1.9-4.2-4.2s1.9-4.2 4.2-4.2 4.2 1.9 4.2 4.2-1.9 4.2-4.2 4.2zm.9-10.3c-.3 0-.6-.1-.9-.1-3.1 0-5.6 2.2-6.1 5.2H37.5C37 65 35 63 32.4 62.5v-25c2.1-.3 3.9-1.8 4.7-3.7l25.4 6v.4c0 2.4 1.4 4.5 3.4 5.5l-4.8 16.8z' />
     </svg>
   )
@@ -180,13 +185,13 @@ export default class Layout extends PureComponent {
           }
 
           .logo {
-            height: 180px;
+            height: 105px;
             margin: 0 auto;
             overflow: hidden;
           }
 
           .footer {
-            padding: 20px 5px;
+            padding: 20px 5px 30px;
             text-align: center;
             color: #fff;
             font-family: 'Lato', sans-serif;
@@ -325,6 +330,11 @@ export default class Layout extends PureComponent {
           </div>
 
           <div className='footer'>
+            <div id='code-sponsor-widget' />
+            <script
+              type='text/javascript'
+              src='https://app.codesponsor.io/scripts/mR5byaXd82L2zTVHo9eWvA?theme=dark&width=240&height=140&image=hide'
+            />
             Created by{' '}
             <a
               target='_blank'
