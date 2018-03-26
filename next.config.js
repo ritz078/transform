7ab8b321c7ccfb54f4b541adccfb1449de412619
Ritesh Kumar <ritz078@users.noreply.github.com>
2018-03-27 02:00:07 +0530

    Fixes (#71)
    * carbon-ads
    
    * fixes #68
    
    * fixes
    
    * fixes #70

--- next.config.js
+++ next.config.js
@@ -28,23 +28,23 @@ module.exports = {
       "babel-core": "babel-standalone"
     };
 
-    if (!dev) {
-      config.plugins.push(
-        new SWPrecacheWebpackPlugin({
-          minify: true,
-          staticFileGlobsIgnorePatterns: [/\.next\//],
-          staticFileGlobs: [
-            "static/**/*" // Precache all static files by default
-          ],
-          runtimeCaching: [
-            {
-              handler: "networkFirst",
-              urlPattern: /^https?.*/
-            }
-          ]
-        })
-      );
-    }
+    // if (!dev) {
+    //   config.plugins.push(
+    //     new SWPrecacheWebpackPlugin({
+    //       minify: true,
+    //       staticFileGlobsIgnorePatterns: [/\.next\//],
+    //       staticFileGlobs: [
+    //         "static/**/*" // Precache all static files by default
+    //       ],
+    //       runtimeCaching: [
+    //         {
+    //           handler: "networkFirst",
+    //           urlPattern: /^https?.*/
+    //         }
+    //       ]
+    //     })
+    //   );
+    // }
 
     if (ANALYZE) {
       config.plugins.push(
