0ea474c7c476bc20bb580f55677138c01c9ee6c9
ritz078 <rkritesh078@gmail.com>
2018-03-29 01:43:30 +0530

    fix-worker-path

--- pages/jsx-to-js-object.js
+++ pages/jsx-to-js-object.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from "react";
 import Layout from "../components/Layout";
 import ConversionPanel from "../components/ConversionPanel";
 import PoweredBy from "../components/PoweredBy";
-import worker from "workerize-loader!../workers/jsx-to-js-object";
+import worker from "workerize-loader?inline!../workers/jsx-to-js-object";
 
 const defaultText = `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100" version="1.1">
    <rect width="200" height="100" stroke="black" stroke-width="6" fill="green"/>
