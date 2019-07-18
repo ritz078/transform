936afb32a4d6b6fe9d8d74b08ccc283d9eee9527
ritz078 <rkritesh078@gmail.com>
2019-07-19 03:41:52 +0530

    fixes

--- components/EditorPanel.tsx
+++ components/EditorPanel.tsx
@@ -107,7 +107,7 @@ export default function({
 
   useEffect(() => {
     // @ts-ignore
-    window.__webpack_public_path__ = "/static/";
+    window.__webpack_public_path__ = "/_next/static/";
   }, []);
 
   const getSettings = useCallback(
