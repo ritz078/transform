eb68108b675d5ab3d995c40a064477038af4e416
Ritesh Kumar <ritz078@users.noreply.github.com>
2019-07-19 11:51:51 +0530

    make monaco workers available in static folder (#106)
    * fixes
    
    * make monaco workers available in static folder
    
    * fixes
    
    * fixes
    
    * fix(worker): monaco workers path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,31 +8,15 @@ interface DocumentProps {
 }
 
 function trackingScript() {
-  (function(i, s, o, g, r, a, m) {
-    i["GoogleAnalyticsObject"] = r;
-    (i[r] =
-      i[r] ||
-      function() {
-        (i[r].q = i[r].q || []).push(arguments);
-      }),
-      // @ts-ignore
-      (i[r].l = 1 * new Date());
-    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-  })(
-    window,
-    document,
-    "script",
-    "https://www.google-analytics.com/analytics.js",
-    "ga"
-  );
+  window["dataLayer"] = window["dataLayer"] || [];
+  function gtag() {
+    window["dataLayer"].push(arguments);
+  }
+  // @ts-ignore
+  gtag("js", new Date());
 
   // @ts-ignore
-  window.ga("create", "UA-60624235-5", "auto");
-  // @ts-ignore
-  window.ga("send", "pageview");
+  gtag("config", "UA-60624235-5");
 }
 
 export default class MyDocument extends Document<DocumentProps> {
@@ -69,6 +53,11 @@ export default class MyDocument extends Document<DocumentProps> {
           <Main />
           {hydrationScript}
           <NextScript />
+
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-60624235-5"
+          />
         </body>
       </html>
     );
