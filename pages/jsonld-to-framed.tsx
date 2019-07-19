42f8c7d747917d0b5ad105d199fd50d378cb71ba
ritz078 <rkritesh078@gmail.com>
2019-07-19 18:16:58 +0530

    feat(jsonld): added flatten, compact, n-quads, expanded and framed
    closes #109

---
+++ pages/jsonld-to-framed.tsx
@@ -0,0 +1,32 @@
+import ConversionPanel, { Transformer } from "@components/ConversionPanel";
+import { useCallback } from "react";
+import * as React from "react";
+import { frame } from "jsonld";
+
+export default function() {
+  const transformer = useCallback<Transformer>(
+    async ({ value, splitEditorValue }) => {
+      const jsonLd = await frame(
+        JSON.parse(value),
+        JSON.parse(splitEditorValue)
+      );
+
+      return JSON.stringify(jsonLd, null, 2);
+    },
+    []
+  );
+
+  return (
+    <ConversionPanel
+      transformer={transformer}
+      editorTitle="JSON-LD"
+      editorDefaultValue="jsonLd"
+      splitTitle={"Frame"}
+      splitLanguage="json"
+      splitEditorDefaultValue="jsonLdContext"
+      editorLanguage="json"
+      resultTitle="Framed"
+      resultLanguage={"json"}
+    />
+  );
+}
