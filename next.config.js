709fe663198ac46eab5800b14cc101555ee4bde9
Tim Neutkens <timneutkens@me.com>
2019-07-23 15:12:06 +0200

    Add temporary fix for window issue

--- next.config.js
+++ next.config.js
@@ -58,6 +58,13 @@ const config = {
 
     config.output.globalObject = 'typeof self !== "object" ? self : this';
 
+    // Temporary fix for https://github.com/zeit/next.js/issues/8071
+    config.plugins.forEach(plugin => {
+      if (plugin.definitions && plugin.definitions["typeof window"]) {
+        delete plugin.definitions["typeof window"];
+      }
+    });
+
     return config;
   },
   target: "serverless"
