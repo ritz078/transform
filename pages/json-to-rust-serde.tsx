a1b0ffe4e690d6ddd41a4384d32c744f9711c3c2
ritz078 <rkritesh078@gmail.com>
2019-07-24 19:57:28 +0530

    chore(rust): remove settings

--- pages/json-to-rust-serde.tsx
+++ pages/json-to-rust-serde.tsx
@@ -1,58 +1,24 @@
 import ConversionPanel, { Transformer } from "@components/ConversionPanel";
 import * as React from "react";
-import { useCallback } from "react";
-import { useSettings } from "@hooks/useSettings";
-import { EditorPanelProps } from "@components/EditorPanel";
-import Form, { InputType } from "@components/Form";
-
-const formFields = [
-  {
-    label: "Convert keys to Snake Case",
-    key: "snakeCase",
-    type: InputType.SWITCH
-  }
-];
+import { useCallback, useEffect } from "react";
 
 export default function() {
-  const name = "JSON to Rust Serde";
+  useEffect(() => {
+    import("@utils/textEncoder");
+  }, []);
 
-  const [settings, setSettings] = useSettings(name, {
-    snakeCase: false
-  });
-
-  const getSettingsElement = useCallback<EditorPanelProps["settingElement"]>(
-    ({ open, toggle }) => {
-      return (
-        <Form<{
-          snakeCase: boolean;
-        }>
-          title={name}
-          onSubmit={setSettings}
-          open={open}
-          toggle={toggle}
-          formsFields={formFields}
-          initialValues={settings}
-        />
-      );
-    },
-    []
-  );
-
-  const transformer = useCallback<Transformer>(
-    async ({ value }) => {
-      const { run } = await import(
-        "@assets/vendor/json-to-rust/json_typegen_wasm"
-      );
-      return run(
-        "Hello",
-        value,
-        JSON.stringify({
-          output_mode: "rust"
-        })
-      );
-    },
-    [settings]
-  );
+  const transformer = useCallback<Transformer>(async ({ value }) => {
+    const { run } = await import(
+      "@assets/vendor/json-to-rust/json_typegen_wasm"
+    );
+    return run(
+      "Hello",
+      value,
+      JSON.stringify({
+        output_mode: "rust"
+      })
+    );
+  }, []);
 
   return (
     <ConversionPanel
@@ -61,8 +27,6 @@ export default function() {
       editorLanguage="json"
       resultTitle="Rust Serde"
       resultLanguage={"rust"}
-      editorSettingsElement={getSettingsElement}
-      settings={settings}
     />
   );
 }
