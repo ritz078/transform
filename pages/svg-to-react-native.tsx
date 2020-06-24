b81d89a90cd6c3d3cd98403cb61bd56d02876a6a
ritz078 <rkritesh078@gmail.com>
2020-06-25 04:18:14 +0530

    fix(expo): add react-native-svg as a dependency

--- pages/svg-to-react-native.tsx
+++ pages/svg-to-react-native.tsx
@@ -40,6 +40,9 @@ export default function() {
       });
 
       snackSession.current.startAsync();
+      await snackSession.current.addDependencies({
+        "react-native-svg": "12.x.x"
+      });
       const url = await snackSession.current.getUrlAsync();
       setUrl(url);
     })();
