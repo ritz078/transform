ee9ec0beb917214c6ce1f68dfcaaa9b0ca7ace1c
Miklos Megyes <m.miklos05@gmail.com>
2020-09-28 01:54:16 +0200

    update test data

--- constants/data.ts
+++ constants/data.ts
@@ -41,115 +41,75 @@ li {
 }
 `;
 
-// export const css2 = `.alert {
-//   position: relative;
-//   padding: 1.6rem 4.6rem;
-//   margin-bottom: 1.6rem;
-//   border: 1px solid #9ae6b4;
-//   color: #fff;
-//   border-radius: 0.2rem;
-//   width: 100%;
-// }
-
-// .logo {
-//   margin-bottom: 1.6rem;
-//   background: url('logo.svg') no-repeat;
-//   display: flex;
-//   justify-content: center;
-// }
-
-// .separator {
-//   background: unset;
-// }
-
-// .container {
-//   background: #ffffff;
-//   border: 1px solid #fff5f5;
-//   border-radius: 0.2rem;
-// }
-
-// .header {
-//   font-weight: 400;
-//   font-size: 2rem;
-//   letter-spacing: 0.03rem;
-//   padding: 2.4rem;
-//   border-bottom: 1px solid #fff5f5;
-// }
-
-// .footer {
-//   width: 100%;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   flex-direction: row-reverse;
-//   padding: 2.4rem 3rem;
-//   border-top: 1px solid #fff5f5;
-// }
-
-// .content--with-side-content {
-//   width: 50%;
-// }
-
-// .content--with-separator {
-//   border-right: 1px solid #fff5f5;
-// }
-
-// .content-inner {
-//   width: 100%;
-//   display: flex;
-//   justify-content: center;
-//   flex-direction: column;
-//   margin: 0;
-//   padding: 3rem;
-// }
-
-// .side-content {
-//   width: 50%;
-//   background: #425634;
-//   padding: 3rem;
-//   display: flex;
-//   justify-content: center;
-//   flex-direction: column;
-// }
-// `;
-
-export const css2 = `.baz {
-  padding: 1.6rem 4.6rem;
-  background: url('logo.svg') no-repeat;
-}
-
-.foo {
+export const css2 = `.alert {
   position: relative;
   padding: 1.6rem 4.6rem;
   margin-bottom: 1.6rem;
-  border: 1px solid #FAD0D0;
+  border: 1px solid #c53030;
   color: #fff;
   border-radius: 0.2rem;
   width: 100%;
-} 
+}
 
-.bar {
-  position: relative;
+.logo {
+  margin-bottom: 1.6rem;
+  background: url('logo.svg') no-repeat;
+  display: flex;
+  justify-content: center;
+}
+
+.button {
+  background: #81e6d9;
   padding: 1.6rem 4.6rem;
+  letter-spacing: 0.03rem;
+  border-radius: 0.2rem;
 }
 
-.foo:hover {
-  background: #cccccc;
+.button:hover {
+  background: #2c7a7b;
 }
 
-.foo:focus {
-  border-color: #e2f2f0;
+@media (min-width: 640px) {
+  .button {
+    padding: 0.5rem 1rem;
+    width: 100%;
+  } 
 }
 
 @media (min-width: 1280px) {
-  .bar {
+  .button {
     padding: 3rem 7rem;
     margin-bottom: 2.4rem;
   } 
 }
 
-.bar::placeholder {
-  color: #e6e6e6;
+.username {
+  color: #718096;
+  border-color: #bee3f8;
+}
+
+.username:focus {
+  border-color: #3182ce;
+}
+
+.username::placeholder {
+  color: #cbd5e0;
+}
+
+@media (min-width: 1280px) {
+  .username {
+    width: 50%;
+  } 
+}
+
+.footer {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row-reverse;
+  padding: 2.4rem 3rem;
+  border-top: 1px solid #fff5f5;
 }
 `;
 
