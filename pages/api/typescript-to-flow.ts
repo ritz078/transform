54710f15c56e9fcd232f4a43f19e787da7ae14bd
ritz078 <rkritesh078@gmail.com>
2020-09-29 07:55:25 +0530

    fix errors

--- pages/api/typescript-to-flow.ts
+++ pages/api/typescript-to-flow.ts
@@ -7,6 +7,8 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     const result = compiler.compileDefinitionString(req.body);
     res.status(200).send(beautify(result));
   } catch (e) {
+    console.log(e);
+
     res.status(500).send(e.message);
   }
 };
--- pages/api/typescript-to-json-schema.ts
+++ pages/api/typescript-to-json-schema.ts
@@ -1,11 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Config } from "ts-json-schema-generator/dist/src/Config";
-import tempy from "tempy";
 import * as tsj from "ts-json-schema-generator";
+import os from "os";
+import crypto from "crypto";
+import path from "path";
+import fs from "fs";
+
+const tmpDir = os.tmpdir?.();
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  const filePath =
+    path.join(tmpDir, crypto.randomBytes(16).toString("hex")) + ".ts";
   try {
-    const filePath = tempy.writeSync(req.body, { extension: "ts" });
+    fs.writeFileSync(filePath, req.body, {
+      encoding: "utf-8"
+    });
     const config: Config = {
       path: filePath,
       expose: "all",
@@ -18,4 +27,5 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   } catch (e) {
     res.status(500).send(e.message);
   }
+  fs.unlinkSync(filePath);
 };
