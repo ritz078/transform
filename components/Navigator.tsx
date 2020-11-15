365bbad91c9b6bd4d16bbdedcd8a2b43103b8958
ritz078 <rkritesh078@gmail.com>
2020-11-15 16:35:05 +0530

    add buy me a coffee

--- components/Navigator.tsx
+++ components/Navigator.tsx
@@ -75,6 +75,17 @@ export default function() {
       </Pane>
 
       <Pane borderTop>
+        <a href="https://www.buymeacoffee.com/ritz078" target="_blank">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            style={{
+              height: 40,
+              display: "block",
+              margin: "10px auto 0"
+            }}
+          />
+        </a>
         <Heading size={400} paddingY={15} textAlign="center">
           Created by{" "}
           <a href="https://twitter.com/ritz078" target="_blank">
