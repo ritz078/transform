2f926125a3cb15dbdf978a8c8714fddc5962725b
John Schulz <john.schulz@elastic.co>
2021-02-21 09:51:53 -0500

    Move OpenAPI conversion to api

---
+++ pages/api/json-schema-to-openapi-schema.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import toOpenApi from "@openapi-contrib/json-schema-to-openapi-schema";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const jsonSchema = req.body;
+    const openApiSchema = await toOpenApi(JSON.parse(jsonSchema), {
+      cloneSchema: true
+    });
+
+    res.status(200).send(JSON.stringify(openApiSchema, null, 2));
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
