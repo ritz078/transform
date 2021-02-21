c0867beff19600a4d616badf46693c52d5567c46
John Schulz <github.com@jfsiii.org>
2021-02-21 10:22:17 -0500

    Fix 500 for json schema to openapi schema (#268)
    * Use same package in packageUrl, view, package.json
    
    Trying a hunch for https://github.com/ritz078/transform/issues/247
    
    Either way, I don't know how these relate but it seems like they should all be the same. I also can't find any other reference to the wework package.
    
    * Move OpenAPI conversion to api

--- pages/json-schema-to-openapi-schema.tsx
+++ pages/json-schema-to-openapi-schema.tsx
@@ -1,18 +1,12 @@
 import ConversionPanel, { Transformer } from "@components/ConversionPanel";
 import * as React from "react";
 import { useCallback } from "react";
-import toOpenApi from "@openapi-contrib/json-schema-to-openapi-schema";
+import request from "@utils/request";
+import { Alert } from "evergreen-ui";
 
 export default function() {
-  const transformer = useCallback<Transformer>(
-    async ({ value }) =>
-      JSON.stringify(
-        toOpenApi(JSON.parse(value), {
-          cloneSchema: true
-        }),
-        null,
-        2
-      ),
+  const transformer = useCallback(
+    ({ value }) => request("/api/json-schema-to-openapi-schema", value),
     []
   );
 
@@ -24,6 +18,14 @@ export default function() {
       editorDefaultValue="jsonSchema"
       resultTitle="Open API Schema"
       resultLanguage={"json"}
+      resultEditorProps={{
+        topNotifications: () => (
+          <Alert
+            backgroundColor="#e7f7ff"
+            title="This code is converted on the server."
+          />
+        )
+      }}
     />
   );
 }
