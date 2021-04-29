5826e26484f2998f14a45809e159e7bbf7bde8b7
John Schulz <john.schulz@elastic.co>
2021-04-29 09:02:15 -0400

    WIP. Logging

--- pages/api/typescript-to-json-schema.ts
+++ pages/api/typescript-to-json-schema.ts
@@ -9,27 +9,44 @@ import fs from "fs";
 const tmpDir = os.tmpdir?.();
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const filePath =
+  const programPath =
     path.join(tmpDir, crypto.randomBytes(16).toString("hex")) + ".ts";
+
   try {
     const program = req.body;
-    fs.writeFileSync(filePath, program, {
+    fs.writeFileSync(programPath, program, {
       encoding: "utf-8"
     });
     const config: Config = {
-      path: filePath,
+      path: programPath,
+      tsconfig: "tsconfig.json",
       expose: "all",
       jsDoc: "extended",
       type: "*"
     };
 
-    const schema = createGenerator(config).createSchema(config.type);
-    const read = fs.readFileSync(filePath, { encoding: "utf-8" });
-    res
-      .status(200)
-      .send(JSON.stringify({ filePath, program, read, schema }, null, 2));
+    console.log({ config });
+    // const schema = createGenerator(config).createSchema(config.type);
+    const read = fs.readFileSync(programPath, { encoding: "utf-8" });
+    res.status(200).send(
+      JSON.stringify(
+        {
+          programPath,
+          program,
+          read,
+          // schema,
+          config,
+          files: fs.readdirSync("."),
+          tsconfig: fs.readFileSync("tsconfig.json", { encoding: "utf-8" }),
+          "next-env": fs.readFileSync("next-env.d.ts", { encoding: "utf-8" }),
+          typings: fs.readFileSync("typings.d.ts", { encoding: "utf-8" })
+        },
+        null,
+        2
+      )
+    );
   } catch (e) {
     res.status(500).send(e.message);
   }
-  fs.unlinkSync(filePath);
+  fs.unlinkSync(programPath);
 };
