3a9479dcedc30d855e317535950f1265021372e2
“Pooja <pakcutie14@gmail.com>
2022-12-29 16:10:20 +0530

    Added constructor generation in Java model classes

--- pages/json-to-java.tsx
+++ pages/json-to-java.tsx
@@ -15,26 +15,60 @@ export default function JsonToJava() {
 
     // use Kotlin transformation to convert JSON to Java
     let javaTransformation: string = "";
-    let getters: string = "";
-    let setters: string = "";
+    let currentClass: string = "";
+    let variableNames: string[] = [];
+    let variableTypes: string[] = [];
 
     kotlinTransformationLines.forEach((line: string) => {
       line = line.trim();
 
       if (line === ")") {
-        // Class is closing so append the getters, setters for the current class,
-        // close the class and reset the getters, setters
-        javaTransformation += `\n${getters}${setters}}`;
-        getters = "";
-        setters = "";
+        // Class is closing so generate constructor, getters and setters for
+        // the current class, close the class and reset running values
+        let args: string[] = [];
+        let getters: string[] = [];
+        let setters: string[] = [];
+
+        // Create args for constructor, getters and setters
+        for (let i = 0; i < variableNames.length; i++) {
+          const type = variableTypes[i];
+          const variableName = variableNames[i];
+          const titleCaseVariable =
+            variableName.charAt(0).toUpperCase() + variableName.substring(1);
+          args.push(`${type} ${variableName}`);
+          getters.push(
+            `\tpublic ${type} get${titleCaseVariable}() {\n\t\treturn this.${variableName};\n\t}\n`
+          );
+          setters.push(
+            `\tpublic void set${titleCaseVariable}(${type} ${variableName}) {\n\t\tthis.${variableName} = ${variableName};\n\t}\n`
+          );
+        }
+
+        // Create constructor
+        let constructor = `\tpublic ${currentClass}(${args.join(", ")}) {`;
+        let properties: string[] = [];
+        variableNames.forEach(variable => {
+          properties.push(`this.${variable} = ${variable};`);
+        });
+        constructor += `\n\t\t${properties.join("\n\t\t")}\n\t}\n`;
+        javaTransformation += `\n${constructor}\n${getters.join(
+          "\n"
+        )}\n${setters.join("\n")}}`;
+
+        // Reset running values
+        currentClass = "";
+        variableNames = [];
+        variableTypes = [];
       } else if (line.startsWith("data class ")) {
         // Change the start of a class from 'data class Root(' to 'public class Root {'
-        const classNameIndex = 11;
+        const classNameStartIndex = 11;
         const classNameEndIndex = line.indexOf("(");
-        javaTransformation += `public class ${line.substring(
-          classNameIndex,
+        const className = line.substring(
+          classNameStartIndex,
           classNameEndIndex
-        )} {`;
+        );
+        javaTransformation += `public class ${className} {`;
+        currentClass = className;
       } else if (line.startsWith("val")) {
         // If this is a variable, change 'val name: String' to 'private String name;'
         // followed by respective getters, setters for the variable
@@ -50,10 +84,10 @@ export default function JsonToJava() {
           typeStartIndex,
           processedLine.length - 1
         );
-        const titleCaseVariable =
-          variable.charAt(0).toUpperCase() + variable.substring(1);
-        getters += `\tpublic ${type} get${titleCaseVariable}() {\n\t\treturn this.${variable};\n\t}\n\n`;
-        setters += `\tpublic void set${titleCaseVariable}(${type} ${variable}) {\n\t\tthis.${variable} = ${variable};\n\t}\n\n`;
+
+        // Save variables and their types to be later used in constructor, getter, setter generation
+        variableNames.push(variable);
+        variableTypes.push(type);
         javaTransformation += `\tprivate ${type} ${variable};`;
       } else if (line.startsWith("typealias")) {
         // If this is a kotlin typealias, make it into a class
@@ -67,13 +101,13 @@ export default function JsonToJava() {
         const type = line.substring(typeNameEndIndex, line.length - 1); // ignore the semi-colon
         const variable =
           className.charAt(0).toLowerCase() + className.substring(1);
+
         const titleCaseVariable = className;
-        getters = `\tpublic ${type} get${titleCaseVariable}() {\n\t\treturn this.${variable};\n\t}\n\n`;
-        setters = `\tpublic void set${titleCaseVariable}(${type} ${variable}) {\n\t\tthis.${variable} = ${variable};\n\t}\n\n`;
+        const getters = `\tpublic ${type} get${titleCaseVariable}() {\n\t\treturn this.${variable};\n\t}\n\n`;
+        const setters = `\tpublic void set${titleCaseVariable}(${type} ${variable}) {\n\t\tthis.${variable} = ${variable};\n\t}\n\n`;
+        const constructor = `\tpublic ${className}(${type} ${variable}) {\n\t\tthis.${variable} = ${variable}\n\t}\n`;
         javaTransformation += `public class ${className} {\n\tprivate ${type} ${variable};\n`;
-        javaTransformation += `\n${getters}${setters}}`;
-        getters = "";
-        setters = "";
+        javaTransformation += `\n${constructor}\n${getters}${setters}}`;
       } else {
         // If there's any other line, it is most probably a 'next line character', so just append it
         javaTransformation += line;
