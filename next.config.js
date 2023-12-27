b6005b2db04f7a59b778a9df71c6ab3fb84db719
Ritesh Kumar <ritz078@users.noreply.github.com>
2023-12-28 02:28:06 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
